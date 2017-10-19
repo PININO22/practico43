771af419e60fbfa48c0a6123ae6ef92370573631
TricaricoJuan <Tricampeon98@gmail.com>
2017-10-19 20:01:52 -0300

    Heredadas de Telefono

---
+++ JavaApplication16/src/Entidades/Fijo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Fijo extends Telefono{
+    private String tipoDeLinea;
+
+    public String getTipoDeLinea() {
+        return tipoDeLinea;
+    }
+
+    public void setTipoDeLinea(String tipoDeLinea) {
+        this.tipoDeLinea = tipoDeLinea;
+    }
+    
+    
+}
---
+++ JavaApplication16/src/Entidades/Inalambrico.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Inalambrico extends Telefono{
+    private int canal;
+    private double alcance;
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public double getAlcance() {
+        return alcance;
+    }
+
+    public void setAlcance(double alcance) {
+        this.alcance = alcance;
+    }
+    
+    
+    
+    
+}
